7f522621e2afe4cb31d29c63956011ca5a6f7ca1
hendisantika <hendisantika@yahoo.co.id>
2025-04-22 06:58:42 +0700

    feat: Add securityFilterChain in SecurityConfiguration🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/twofaemailjwt/config/jwt/SecurityConfiguration.java
+++ src/main/java/id/my/hendisantika/twofaemailjwt/config/jwt/SecurityConfiguration.java
@@ -4,9 +4,17 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Arrays;
 
 /**
  * Created by IntelliJ IDEA.
@@ -35,5 +43,20 @@ public class SecurityConfiguration {
     @Bean
     public UserDetailsService userDetailsService() {
         return customUserDetailService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(Arrays.stream(ApiPathExclusion.values())
+                                .map(ApiPathExclusion::getPath)
+                                .toArray(String[]::new)).permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
