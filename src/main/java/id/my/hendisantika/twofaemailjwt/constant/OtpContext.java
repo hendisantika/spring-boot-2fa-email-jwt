cbe4b611abdca8cbe1bf7334a382e50a87b4890e
hendisantika <hendisantika@yahoo.co.id>
2025-04-21 22:37:07 +0700

    feat: Add OtpContext🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/twofaemailjwt/constant/OtpContext.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.twofaemailjwt.constant;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-2fa-email-jwt
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/04/25
+ * Time: 22.36
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OtpContext {
+    SIGN_UP, LOGIN, ACCOUNT_DELETION
+}
