48f5f642023f2c0a8451f288f76aa91defaa615c
hendisantika <hendisantika@yahoo.co.id>
2025-04-21 22:33:49 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/twofaemailjwt/SpringBoot2faEmailJwtApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.twofaemailjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2faEmailJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2faEmailJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/twofaemailjwt/SpringBoot2faEmailJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.twofaemailjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2faEmailJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
