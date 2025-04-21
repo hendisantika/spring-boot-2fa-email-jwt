0b43e63ba0f364cdbad15eed010ddcdeb7529a19
hendisantika <hendisantika@yahoo.co.id>
2025-04-22 06:47:42 +0700

    feat: Add LoginSuccessDto🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/twofaemailjwt/dto/LoginSuccessDto.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.twofaemailjwt.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-2fa-email-jwt
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 22/04/25
+ * Time: 06.47
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Builder
+@Jacksonized
+public class LoginSuccessDto {
+    private final String accessToken;
+    private final String refreshToken;
+}
