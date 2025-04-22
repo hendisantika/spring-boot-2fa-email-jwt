e2caeadde21bd6a2488e5383f5504ccfd496d2df
hendisantika <hendisantika@yahoo.co.id>
2025-04-22 07:17:35 +0700

    feat: Add userAccountCreationHandler in AuthenticationController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/twofaemailjwt/controller/AuthenticationController.java
+++ src/main/java/id/my/hendisantika/twofaemailjwt/controller/AuthenticationController.java
@@ -1,8 +1,16 @@
 package id.my.hendisantika.twofaemailjwt.controller;
 
+import id.my.hendisantika.twofaemailjwt.dto.SignupRequestDto;
 import id.my.hendisantika.twofaemailjwt.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,4 +30,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final UserService userService;
+
+    @PostMapping(value = "/sign-up", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    @Operation(summary = "Creates a user account in the system")
+    public ResponseEntity<?> userAccountCreationHandler(
+            @RequestBody(required = true) final SignupRequestDto userAccountCreationRequestDto) {
+        return userService.createAccount(userAccountCreationRequestDto);
+    }
 }
