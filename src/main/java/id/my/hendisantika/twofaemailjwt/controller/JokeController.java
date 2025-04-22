a7b3da2e7b3e6f06544ff6b7fd4b00b7050f9312
hendisantika <hendisantika@yahoo.co.id>
2025-04-22 07:19:34 +0700

    feat: Add jokeRetrievalHandler in JokeController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/twofaemailjwt/controller/JokeController.java
+++ src/main/java/id/my/hendisantika/twofaemailjwt/controller/JokeController.java
@@ -1,8 +1,17 @@
 package id.my.hendisantika.twofaemailjwt.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +28,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class JokeController {
+    @GetMapping(value = "/joke", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    @Operation(summary = "Returns a funny joke")
+    public ResponseEntity<?> jokeRetrievalHandler() {
+
+        final var response = new JSONObject();
+        try {
+            response.put("joke", "What kind of murderer has moral fiber? – A cereal killer.");
+        } catch (JSONException e) {
+            log.error("Unable to generate JSON response");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return ResponseEntity.ok(response.toString());
+    }
 }
