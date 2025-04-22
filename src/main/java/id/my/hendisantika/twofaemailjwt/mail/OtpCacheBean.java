ca2a85c75a00359267085185a02294a245b82faf
hendisantika <hendisantika@yahoo.co.id>
2025-04-22 07:34:22 +0700

    feat: Add oneTimePasswordCache🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/twofaemailjwt/mail/OtpCacheBean.java
+++ src/main/java/id/my/hendisantika/twofaemailjwt/mail/OtpCacheBean.java
@@ -1,8 +1,14 @@
 package id.my.hendisantika.twofaemailjwt.mail;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +27,18 @@ import org.springframework.context.annotation.Configuration;
 public class OtpCacheBean {
 
     private final OneTimePasswordConfigurationProperties oneTimePasswordConfigurationProperties;
+
+    @Bean
+    public LoadingCache<String, Integer> oneTimePasswordCache() {
+        return CacheBuilder.newBuilder()
+                .expireAfterWrite(oneTimePasswordConfigurationProperties.getOtp().getExpirationMinutes(), TimeUnit.MINUTES)
+                .build(new CacheLoader<>() {
+                    @Override
+                    public Integer load(String key) {
+                        return 0; // Default value, not used in practice as we explicitly put values
+                    }
+                });
+    }
+
 
 }
