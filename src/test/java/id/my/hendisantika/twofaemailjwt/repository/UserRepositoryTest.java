6a4c7c924fa3247d2999ea66896caf000b0da930
hendisantika <hendisantika@yahoo.co.id>
2025-04-23 07:11:46 +0700

    feat: Add findByEmailId_WhenUserExists_ShouldReturnUser in UserRepositoryTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/twofaemailjwt/repository/UserRepositoryTest.java
+++ src/test/java/id/my/hendisantika/twofaemailjwt/repository/UserRepositoryTest.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -39,5 +42,20 @@ public class UserRepositoryTest {
 
         // Assert
         assertFalse(exists);
+    }
+
+    @Test
+    void findByEmailId_WhenUserExists_ShouldReturnUser() {
+        // Arrange
+        User user = createTestUser("test@example.com");
+        entityManager.persist(user);
+        entityManager.flush();
+
+        // Act
+        Optional<User> foundUser = userRepository.findByEmailId("test@example.com");
+
+        // Assert
+        assertTrue(foundUser.isPresent());
+        assertEquals("test@example.com", foundUser.get().getEmailId());
     }
 }
