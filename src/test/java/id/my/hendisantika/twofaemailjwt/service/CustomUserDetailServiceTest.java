c18fcfb2d7dc6b373b8be85b37136151c8a396eb
hendisantika <hendisantika@yahoo.co.id>
2025-04-23 07:10:06 +0700

    feat: Add loadUserByUsername_WhenUserDoesNotExist_ShouldThrowException in CustomUserDetailServiceTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/twofaemailjwt/service/CustomUserDetailServiceTest.java
+++ src/test/java/id/my/hendisantika/twofaemailjwt/service/CustomUserDetailServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -16,6 +17,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
@@ -53,4 +55,21 @@ public class CustomUserDetailServiceTest {
         assertEquals(user.getPassword(), userDetails.getPassword());
         assertTrue(userDetails.getAuthorities().isEmpty());
     }
+
+    @Test
+    void loadUserByUsername_WhenUserDoesNotExist_ShouldThrowException() {
+        // Arrange
+        UUID userId = UUID.randomUUID();
+        String userIdString = userId.toString();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        Exception exception = assertThrows(UsernameNotFoundException.class, () -> {
+            customUserDetailService.loadUserByUsername(userIdString);
+        });
+
+        assertEquals("Bad Credentials", exception.getMessage());
+    }
+
 }
