24f1385f9c30e8edb43c4f7232caa7509be0c102
hendisantika <hendisantika@yahoo.co.id>
2025-04-23 07:13:12 +0700

    feat: Add BeforeEach in OtpCacheBeanTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/twofaemailjwt/mail/OtpCacheBeanTest.java
+++ src/test/java/id/my/hendisantika/twofaemailjwt/mail/OtpCacheBeanTest.java
@@ -1,9 +1,12 @@
 package id.my.hendisantika.twofaemailjwt.mail;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class OtpCacheBeanTest {
@@ -19,5 +22,9 @@ public class OtpCacheBeanTest {
     @InjectMocks
     private OtpCacheBean otpCacheBean;
 
-
+    @BeforeEach
+    void setUp() {
+        when(oneTimePasswordConfigurationProperties.getOtp()).thenReturn(otp);
+        when(otp.getExpirationMinutes()).thenReturn(TEST_EXPIRATION_MINUTES);
+    }
 }
