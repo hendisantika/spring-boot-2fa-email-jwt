bc3609c14adbedf8f15bdb1e88d514470e086220
hendisantika <hendisantika@yahoo.co.id>
2025-04-23 07:16:56 +0700

    feat: Add JokeControllerTest🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/twofaemailjwt/controller/JokeControllerTest.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.twofaemailjwt.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class JokeControllerTest {
+
+    @InjectMocks
+    private JokeController jokeController;
+
+
+}
