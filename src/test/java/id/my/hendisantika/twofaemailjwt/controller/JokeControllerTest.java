b9baf8c5bcea2cd0c06d01612ceb70389627f0cf
hendisantika <hendisantika@yahoo.co.id>
2025-04-23 07:17:25 +0700

    feat: Add jokeRetrievalHandler_ShouldReturnJoke in JokeControllerTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/twofaemailjwt/controller/JokeControllerTest.java
+++ src/test/java/id/my/hendisantika/twofaemailjwt/controller/JokeControllerTest.java
@@ -1,8 +1,15 @@
 package id.my.hendisantika.twofaemailjwt.controller;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 public class JokeControllerTest {
@@ -10,5 +17,19 @@ public class JokeControllerTest {
     @InjectMocks
     private JokeController jokeController;
 
+
+    @Test
+    void jokeRetrievalHandler_ShouldReturnJoke() {
+        // Act
+        ResponseEntity<?> response = jokeController.jokeRetrievalHandler();
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+
+        String responseBody = (String) response.getBody();
+        assertTrue(responseBody.contains("joke"));
+        assertTrue(responseBody.contains("cereal killer"));
+    }
 
 }
