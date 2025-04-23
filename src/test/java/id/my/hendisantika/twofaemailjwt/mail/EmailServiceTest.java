8954e86056873d312a74110fd949bd3397fab9e0
hendisantika <hendisantika@yahoo.co.id>
2025-04-23 07:14:11 +0700

    feat: Add EmailServiceTest🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/twofaemailjwt/mail/EmailServiceTest.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.twofaemailjwt.mail;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailServiceTest {
+
+    private static final String TEST_EMAIL = "recipient@example.com";
+    private static final String TEST_SUBJECT = "Test Subject";
+    private static final String TEST_MESSAGE = "Test Message Body";
+    private static final String SENDER_EMAIL = "sender@example.com";
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Mock
+    private EmailConfigurationProperties emailConfigurationProperties;
+
+    @InjectMocks
+    private EmailService emailService;
+
+    @Captor
+    private ArgumentCaptor<SimpleMailMessage> mailMessageCaptor;
+
+
+}
