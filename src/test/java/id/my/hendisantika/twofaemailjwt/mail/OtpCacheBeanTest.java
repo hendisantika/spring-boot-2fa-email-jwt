68e93da5ed2de788f508ac7e0a60cff06d45b0c7
hendisantika <hendisantika@yahoo.co.id>
2025-04-23 07:13:32 +0700

    feat: Add oneTimePasswordCache_ShouldCreateCacheWithCorrectExpiration in OtpCacheBeanTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/twofaemailjwt/mail/OtpCacheBeanTest.java
+++ src/test/java/id/my/hendisantika/twofaemailjwt/mail/OtpCacheBeanTest.java
@@ -1,11 +1,17 @@
 package id.my.hendisantika.twofaemailjwt.mail;
 
+import com.google.common.cache.LoadingCache;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,5 +32,21 @@ public class OtpCacheBeanTest {
     void setUp() {
         when(oneTimePasswordConfigurationProperties.getOtp()).thenReturn(otp);
         when(otp.getExpirationMinutes()).thenReturn(TEST_EXPIRATION_MINUTES);
+    }
+
+    @Test
+    void oneTimePasswordCache_ShouldCreateCacheWithCorrectExpiration() throws ExecutionException {
+        // Act
+        LoadingCache<String, Integer> cache = otpCacheBean.oneTimePasswordCache();
+
+        // Assert
+        assertNotNull(cache);
+
+        // Verify default value
+        assertEquals(0, cache.get("test_key").intValue());
+
+        // Put a value and verify it can be retrieved
+        cache.put("test_key", 123456);
+        assertEquals(123456, cache.get("test_key").intValue());
     }
 }
